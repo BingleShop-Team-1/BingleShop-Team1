590d073b787d8f6b0b59f13c0bdcd7fac9185963
DonCarlo3105 <khusnudhoni@gmail.com>
2024-03-26 00:17:34 +0700

    [BPT1-16] feat: Sudah dikerjakan user.router.js di dalam folder router. code ini sudah dilengkapi validasi mungkin ada yang kurang mohon dikabari ya saya.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const { User, Order, Item } = require('./models')
+const { userRouter } = require('/Routes/user.router')
 
 const app = express()
 const port = 3002
@@ -13,74 +14,16 @@ app.set('view engine', 'ejs')
 
 const statusOrder = ['pending', 'success']
 
-app.get('/login', (req, res) => res.render('login'))
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+app.use('/login', userRouter)
 
-    const user = await User.findOne({ where: { email: email } });
+app.use("/register", userRouter)
 
-    if (user != undefined) {
-        const isValidPassword = bcrypt.compareSync(password, user.password)
-        if (isValidPassword) {
-            return res.sendStatus(200)
-        }
+app.use('/users', userRouter)
 
-        return res.status(422).json({
-            status: false,
-            error: "Wrong email or password"
-        })
-    }
+app.use('/users', userRouter)
 
-    return res.status(422).json({
-        status: false,
-        error: "Email is not registered"
-    })
-})
+app.use('/users/:id', userRouter)
 
-app.get('/register', (req, res) => res.render('register'))
-app.post("/register", async (req, res) => {
-    const { name, address, email, password } = req.body
-
-    const user = new User
-    user.name = name
-    user.email = email
-    user.address = address
-    user.password = bcrypt.hashSync(password, 10)
-    await user.save()
-
-    return res.sendStatus(201)
-})
-
-app.get('/users', (req, res) => getList(req, res, User))
-app.post('/users', async (req, res) => {
-    const { name, address, email, password } = req.body
-
-    const user = new User
-    user.name = name
-    user.email = email
-    user.address = address
-    user.password = bcrypt.hashSync(password, 10)
-    await user.save()
-
-    return res.sendStatus(201)
-})
-app.put('/users/:id', async (req, res) => {
-    const id = req.params.id
-    const { name, address, email, password } = req.body
-
-    const user = await User.findByPk(id)
-    if (user != undefined) {
-        user.name = name
-        user.email = email
-        user.address = address
-        user.password = bcrypt.hashSync(password, 10)
-        await user.save()
-
-        return res.sendStatus(204)
-    }
-
-    return res.sendStatus(404)
-})
 app.delete('/users/:id', async (req, res) => {
     const id = req.params.id
 

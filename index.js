e8c34a9f9414cd930fa9f450129034b1f0c4ca8d
DonCarlo3105 <khusnudhoni@gmail.com>
2024-03-26 21:30:58 +0700

    [BPT1-16] feat: Menambah fitur pelanggan

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const { User, Order, Item } = require('./models')
-var ordersRouter = require('./routes/orders');
+const { userRouter } = require('/Routes/user.router')
 
 const app = express()
 const port = 3002
@@ -14,74 +14,16 @@ app.set('view engine', 'ejs')
 
 const statusOrder = ['pending', 'success']
 
-app.get('/login', (req, res) => res.render('login'))
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+app.use('/login', userRouter)
 
-    const user = await User.findOne({ where: { email: email } });
+app.use("/register", userRouter)
 
-    if (user != undefined) {
-        const isValidPassword = bcrypt.compareSync(password, user.password)
-        if (isValidPassword) {
-            return res.sendStatus(200)
-        }
+app.use('/users', userRouter)
 
-        return res.status(422).json({
-            status: false,
-            error: "Wrong email or password"
-        })
-    }
+app.use('/users', userRouter)
 
-    return res.status(422).json({
-        status: false,
-        error: "Email is not registered"
-    })
-})
+app.use('/users/:id', userRouter)
 
-app.get('/register', (req, res) => res.render('register'))
-app.post("/register", async (req, res) => {
-    const { name, address, email, password } = req.body
-
-    const user = new User
-    user.name = name
-    user.email = email
-    user.address = address
-    user.password = bcrypt.hashSync(password, 10)
-    await user.save()
-
-    return res.sendStatus(201)
-})
-
-app.get('/users', (req, res) => getList(req, res, User))
-app.post('/users', async (req, res) => {
-    const { name, address, email, password } = req.body
-
-    const user = new User
-    user.name = name
-    user.email = email
-    user.address = address
-    user.password = bcrypt.hashSync(password, 10)
-    await user.save()
-
-    return res.sendStatus(201)
-})
-app.put('/users/:id', async (req, res) => {
-    const id = req.params.id
-    const { name, address, email, password } = req.body
-
-    const user = await User.findByPk(id)
-    if (user != undefined) {
-        user.name = name
-        user.email = email
-        user.address = address
-        user.password = bcrypt.hashSync(password, 10)
-        await user.save()
-
-        return res.sendStatus(204)
-    }
-
-    return res.sendStatus(404)
-})
 app.delete('/users/:id', async (req, res) => {
     const id = req.params.id
 
@@ -147,7 +89,41 @@ app.delete('/items/:id', async (req, res) => {
     return res.sendStatus(404)
 })
 
-app.use('/orders', ordersRouter);
+app.get('/orders', (req, res) => getList(req, res, Order))
+app.post('/orders', async (req, res) => {
+    const { user_id, item_id, quantity } = req.body
+
+    const order = new Order
+    order.user_id = user_id
+    order.item_id = item_id
+    order.quantity = quantity
+    await order.save()
+
+    return res.sendStatus(201)
+})
+app.patch('/orders/:id/update-status', async (req, res) => {
+    const id = req.params.id
+    const { status } = req.body
+
+    checkStatus = statusOrder.includes(status)
+
+    if (! checkStatus) {
+        return res.status(422).json({
+            status: false,
+            error: 'Status not matched'
+        })
+    }
+
+    const order = await Order.findByPk(id)
+    if (order != undefined) {
+        order.status = status
+        await order.save()
+
+        return res.sendStatus(200)
+    }
+
+    return res.sendStatus(404)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)

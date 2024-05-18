32676a110437a640a8f9de489d9b83febdb335c0
Don Carlo <66800518+CarlitoDon@users.noreply.github.com>
2024-05-18 16:17:18 +0700

    Merapihkan code router

--- routers/item.router.js
+++ routers/item.router.js
@@ -5,7 +5,6 @@ const controller = require("../controllers/item.controller");
 const {uploader} = require("../libs/media.handling");
 
 router.get("/", controller.getItems);
-// router.get("/:id", ); //blm buat function get id
 router.post("/", isAdmin, uploader.single("image"), controller.adminCreateItem);
 router.put("/:id", isAdmin, uploader.single("image"), controller.adminUpdateItem);
 router.delete("/:id", isAdmin, controller.adminDeleteItem);

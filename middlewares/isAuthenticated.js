db4af4d574e7a4b42dcf9076b6adace3e7f13854
Mojito <mojjitt0o@gmail.com>
2024-05-23 02:08:35 +0700

    penambahan fitur mengirim kode verifikasi dengan mailer, serta pengecekan is_verified

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,6 +1,32 @@
-const { User } = require('../models');
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET_KEY } = process.env;
+// const { User } = require('../models');
+// const jwt = require('jsonwebtoken');
+// const { JWT_SECRET_KEY } = process.env;
+
+// module.exports = async (req, res, next) => {
+//     const bearerToken = req.headers['authorization'] || "";
+
+//     if (!bearerToken) {
+//         return res.status(401).send({ message: "Unauthorized - No token provided." });
+//     } else if (!bearerToken.startsWith("Bearer ")) {
+//         return res.status(401).send({ message: "Unauthorized - Invalid token format." });
+//     }
+
+//     const accessToken = bearerToken.replace("Bearer ", "");
+//     try {
+//         const payload = jwt.verify(accessToken, JWT_SECRET_KEY);
+//         const user = await User.findByPk(payload.id);
+
+//         if (!user) {
+//             return res.status(401).send({ message: "Unauthorized - No user data found in token." });
+//         }
+
+//         req.user = user;
+//         next();
+//     } catch (error) {
+//         return res.status(401).send({ message: "Unauthorized - Invalid token." });
+//     }
+// };
+
 
 module.exports = async (req, res, next) => {
     const bearerToken = req.headers['authorization'] || "";
@@ -18,6 +44,11 @@ module.exports = async (req, res, next) => {
 
         if (!user) {
             return res.status(401).send({ message: "Unauthorized - No user data found in token." });
+        }
+
+        if (!user.is_verified) {
+            // Jika pengguna belum terverifikasi, berikan respons atau arahkan kembali ke halaman verifikasi
+            return res.status(401).send({ message: "Unauthorized - Email belum diverifikasi." });
         }
 
         req.user = user;

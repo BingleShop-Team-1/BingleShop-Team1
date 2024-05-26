e6b26d619fb15f741509a829fe08a303ef78925b
Mojito <mojjitt0o@gmail.com>
2024-05-26 11:06:38 +0700

    Implement user registration and email verification

--- routers/user.router.js
+++ routers/user.router.js
@@ -3,15 +3,10 @@ const controller = require('../controllers/user.controller');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 
 router.post("/register", controller.userRegister);
-
-router.get('/', isAuthenticated, controller.getUsers);
-
+router.get('/', isAuthenticated, controller.getUsers); 
 router.post("/login", controller.userLogin);
-
 router.put('/:id', isAuthenticated, controller.userUpdate);
-
 router.delete('/:id', isAuthenticated, controller.userDelete);
-
 router.get('/whoami', isAuthenticated, controller.whoAmI);
 
 module.exports = router;

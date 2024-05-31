593b7d8783569a56985f109bcc68fec3b462496b
Don Carlo <66800518+CarlitoDon@users.noreply.github.com>
2024-05-31 20:08:17 +0700

    Cleaned: [Khusnudhoni] bersihkan code yang tercommand

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -25,10 +25,9 @@ const getOrders = async (req, res) => {
                     ]
                 }
             ],
-            order: [['createdAt', 'DESC']] // Urutkan berdasarkan createdAt
+            order: [['createdAt', 'DESC']] 
         });
 
-        // Format data output untuk respons
         const formattedOrders = orderedByUser.map(order => ({
             id: order.id,
             total_price: order.total_price,
@@ -135,83 +134,6 @@ const createOrder = async (req, res) => {
         return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 };
-
-
-// const createOrder =  async (req, res) => {
-//     // const { item_id, quantity } = req.body
-
-//     // console.log(req.user)
-
-//     // const order = new Order
-//     // order.user_id = req.user.id
-//     // order.total_quantity = 0
-//     // order.total_price = 0
-//     // await order.save()
-
-//     // const orderItem = new OrderItem
-//     // orderItem.order_id = order.id
-//     // orderItem.item_id = item_id
-//     // orderItem.quantity = quantity
-//     // await orderItem.save()
-
-//     // return res.sendStatus(201)
-//     const {items, order} = req.body
-//     if (!items || !order) {
-//         return res.status(400).send("Please fill items and order quantity. Example:{\"items\": [{\"id\": 1}], \"order\": [{\"id\": 1, \"quantity\": 20}]")
-//     }
-
-//     const itemIds = items.map((item) => item.id) 
-
-//     const itemDB = await Item.findAll({
-//         where: {id: itemIds},
-//         raw: true,
-//         attributes: ['id', 'price']
-//     })
-  
-//     if (itemDB.length != items.length) {
-//         return res.status(404).json({
-//             error: 'cannot find item(s) requested'
-//         })
-//     }
-//     const mergedItem = []
-    
-//     itemDB.forEach((itemDB) => {
-//         mergedItem.push({...itemDB, ...(order.find(item => itemDB.id == item.id))})
-//     });
-
-//     const totalOrderPrice = mergedItem.reduce((a, b) => a + b.price * b.quantity, 0)
-
-//     const totalOrderQuantityArr = []
-//     order.forEach((index) => totalOrderQuantityArr.push(index.quantity))
-//     const totalOrderQuantity = totalOrderQuantityArr.reduce((a,b) => a + b)
-
-//     if (isNaN(totalOrderPrice) || isNaN(totalOrderQuantity)) {
-//         return res.status(400).send("Please check request body. Example:{\"items\": [{\"id\": 1}], \"order\": [{\"id\": 1, \"quantity\": 20}]")
-//     }
-
-//     const orderDB = await Order.create({
-//         user_id: req.user.id,
-//         total_quantity: totalOrderQuantity,
-//         total_price: Number(totalOrderPrice),
-//         status: "pending"
-//     })
-
-//     const orderDetails = mergedItem.map((item) => {
-//         return {
-//             order_id: orderDB.id,
-//             item_id: item.id,
-//             quantity: item.quantity,
-//         }
-//     })
-
-//     const orderItems = await OrderItem.bulkCreate(orderDetails)
-    
-//     if(orderItems) {
-//         return res.status(201).json(orderItems)
-//     }
-      
-//     return res.sendStatus(500)
-// }
 
 const updateStatusOrder = async (req, res) => {
     const id = req.params.id

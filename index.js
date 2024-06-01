be57de1d22e40b137a9d8ed331d0774336448341
unknown <nisfiarsnf@gmail.com>
2024-06-01 11:00:50 +0700

    websocket ditambahkan

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = require('./app')
 const server = http.createServer(app)
 const port = process.env.NODE_PORT || 3002
 
-server.listen(port, () => console.log(`BingleShop app listening on port ${port}`))
+const listener = server.listen(port, () => console.log(`BingleShop app listening on port ${port}`))
+
+const { Server } = require('socket.io')
+const { socketController } = require('./controllers')
+
+const io = new Server(listener)
+
+socketController.setup(io)
 
 process.on('SIGINT', () => {
     server.close((err) => {
@@ -15,5 +22,16 @@ process.on('SIGINT', () => {
 
         console.log("BingleShop shutdown... see you later :)")
         process.exit(0)
+    })
+})
+
+process.on('SIGTERM', () => {
+    listener.close((err) => {
+        if (err) {
+            console.log(err)
+            process.exit(1)
+        }
+        console.log("BingleShop shutdown... see you later :)")
+        process.exit(1)
     })
 })
